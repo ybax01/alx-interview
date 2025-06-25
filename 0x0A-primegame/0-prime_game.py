b6890c46a4c0a5d87747968c5ad4b8f02e3f3075
Youssef Ben Ayad <youssef1002016@gmail.com>
2025-06-25 16:20:11 +0100

    try4

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,50 +1,40 @@
 #!/usr/bin/python3
-"""Module for Prime Game"""
+"""Prime Game logic module"""
 
 
 def isWinner(x, nums):
-    """
-    Determines the winner of a set of prime number removal games.
-    """
-    # Check for invalid input
-    if x <= 0 or nums is None:
+    """Determines the winner of each game round"""
+    if not nums or x < 1:
         return None
-    if x != len(nums):
-        return None
-    # Initialize scores and array of possible prime numbers
-    ben = 0
-    maria = 0
-    # Create a list 'a' of length sorted(nums)[-1] + 1 with all elements
-    # initialized to 1
-    a = [1 for x in range(sorted(nums)[-1] + 1)]
-    # The first two elements of the list, a[0] and a[1], are set to 0
-    # because 0 and 1 are not prime numbers
-    a[0], a[1] = 0, 0
-    # Use Sieve of Eratosthenes algorithm to generate array of prime numbers
-    for i in range(2, len(a)):
-        rm_multiples(a, i)
-    # Play each round of the game
-    for i in nums:
-        # If the sum of prime numbers in the set is even, Ben wins
-        if sum(a[0:i + 1]) % 2 == 0:
-            ben += 1
+
+    max_n = max(nums)
+
+    # Sieve of Eratosthenes to count primes up to max_n
+    is_prime = [False, False] + [True] * (max_n - 1)
+    for i in range(2, int(max_n ** 0.5) + 1):
+        if is_prime[i]:
+            for j in range(i * i, max_n + 1, i):
+                is_prime[j] = False
+
+    # Precompute number of primes up to each index
+    prime_count = [0] * (max_n + 1)
+    count = 0
+    for i in range(1, max_n + 1):
+        if is_prime[i]:
+            count += 1
+        prime_count[i] = count
+
+    maria_wins = 0
+    ben_wins = 0
+
+    for n in nums:
+        if prime_count[n] % 2 == 1:
+            maria_wins += 1
         else:
-            maria += 1
-    # Determine the winner of the game
-    if ben > maria:
-        return "Ben"
-    if maria > ben:
+            ben_wins += 1
+
+    if maria_wins > ben_wins:
         return "Maria"
+    elif ben_wins > maria_wins:
+        return "Ben"
     return None
-
-
-def rm_multiples(ls, x):
-    """
-    Removes multiples of a prime number from an array of possible prime
-    numbers.
-    """
-    for i in range(2, len(ls)):
-        try:
-            ls[i * x] = 0
-        except (ValueError, IndexError):
-            break
